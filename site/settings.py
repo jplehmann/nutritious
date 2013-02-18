44dcf3448f8f842193520c045649d05c9e29e1b6
John Lehmann <john.lehmann@gmail.com>
2013-02-17 20:27:37 -0600

    tmp

--- site/settings.py
+++ site/settings.py
@@ -1,4 +1,5 @@
-# Django settings for tagz project.
+"""Django settings for tagz project."""
+from unipath import Path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,10 +10,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+PROJECT_ROOT = Path(__file__).ancestor(3)
+print("Proj root ", PROJECT_ROOT);
+print("db: ", PROJECT_ROOT.child("db").child("sqlite3.db"))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': PROJECT_ROOT.child("db").child("sqlite3.db"), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,9 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    "tagz",
 )
 
 # A sample logging configuration. The only tangible logging
