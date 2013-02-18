103cc565e7a3e17d120483cf370f961906a9bb3c
John Lehmann <john.lehmann@gmail.com>
2013-02-17 23:38:15 -0600

    simplified model

--- tagz/admin.py
+++ tagz/admin.py
@@ -1,8 +1,6 @@
 from tagz.models import Tag
 from tagz.models import Reference
-from tagz.models import Annotation
 from django.contrib import admin
 
 admin.site.register(Tag)
 admin.site.register(Reference)
-admin.site.register(Annotation)
--- tagz/models.py
+++ tagz/models.py
@@ -6,17 +6,12 @@ class Tag(models.Model):
       return self.tag
 
 class Reference(models.Model):
+  tag = models.ForeignKey(Tag)
   book = models.CharField(max_length=100)
   chapter = models.IntegerField()
-  def __unicode__(self):
-    return self.book + " " + str(self.chapter)
-
-class Annotation(models.Model):
-  tag = models.ForeignKey(Tag)
-  reference = models.ForeignKey(Reference)
   firstLine = models.IntegerField()
   lastLine = models.IntegerField()
   def __unicode__(self):
-    return "%s @ %s:%d:%d" % (
-      self.tag, self.reference, self.firstLine, self.lastLine)
+    return "%s @ %s %d:%d:%d" % (
+      self.tag, self.book, self.chapter, self.firstLine, self.lastLine)
 
