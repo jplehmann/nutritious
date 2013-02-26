9a8040cbf3d52d7be3243202758e911396ce803a
John Lehmann <john.lehmann@gmail.com>
2013-02-25 20:36:58 -0600

    sort order for reference and tagz for admin

--- tagz/models.py
+++ tagz/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+
 class Tag(models.Model):
   tag = models.CharField(max_length=100)
+
   def __unicode__(self):
       return self.tag
+
+  class Meta:
+      ordering = ["tag"]
+
 
 class Reference(models.Model):
   tag = models.ForeignKey(Tag)
@@ -11,7 +17,14 @@ class Reference(models.Model):
   chapter = models.IntegerField()
   firstLine = models.IntegerField()
   lastLine = models.IntegerField()
+  #created_at = models.DateTimeField(auto_now_add=True, editable=False)
+  #updated_at = models.DateTimeField(auto_now=True, editable=False)
+
   def __unicode__(self):
     return "%s @ %s %d:%d:%d" % (
       self.tag, self.book, self.chapter, self.firstLine, self.lastLine)
+    
+  class Meta:
+      ordering = ["book", "chapter", "firstLine", "tag", "lastLine"]
+
 
