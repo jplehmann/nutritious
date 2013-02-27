ce37c701a14d6bf5b7960b6b9eb95de5e301747a
John Lehmann <john.lehmann@gmail.com>
2013-02-26 20:34:16 -0600

    basic views

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,10 +7,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tagz.views.home', name='home'),
-    # url(r'^tagz/', include('tagz.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^tagz/', include('tagz.foo.urls')),
+    url(r'^tagz/tags/$', 'tagz.views.tags'),
+    url(r'^tagz/refs/$', 'tagz.views.refs'),
+    url(r'^tagz/tags/(?P<tag_name>[^/]+)/$', 'tagz.views.tag'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- tagz/views.py
+++ tagz/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponse
+import tagz.Tag
+import tagz.Reference
+
+
+def tags(request):
+  """ All tags. For each show all refs. """
+  return HttpResponse("Hello, world. You're at the tags index.")
+
+
+def tag(request, tag_name):
+  """ Single tag. For each show other tags on that ref."""
+  return HttpResponse("Hello, world. You're at the tags index., tagname="+ tag_name)
+
+
+def refs(request):
+  """ All refs. """
+  return HttpResponse("Hello, world. You're at the tags index.")
