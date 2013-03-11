ce327b4af47d0a09c55ed262f03ec7102a14f1b5
John Lehmann <john.lehmann@gmail.com>
2013-03-10 21:15:42 -0500

    route all traffic to tagz (try 2)

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,13 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tagz.views.home', name='home'),
-    url(r'^/$', 'tagz.views.tags'), # redir to tags
-    url(r'^tagz/$', 'tagz.views.tags'), # redir to tags
     url(r'^tagz/tags/$', 'tagz.views.tags'),
     url(r'^tagz/tags/(?P<tag_name>[^\/]+)/$', 'tagz.views.tag'),
     url(r'^tagz/refs/$', 'tagz.views.refs'),
     url(r'^tagz/refs/(?P<ref_name>[^\/]+)/$', 'tagz.views.ref'),
+    url(r'^tagz/$', 'tagz.views.tags'), # redir to tags
+    url(r'^$', 'tagz.views.tags'), # redir to tags
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
