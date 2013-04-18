436a54d6ebda982fa9fc7cffe9e3ee00c9b5b353
John Lehmann <jlehmann@zixcorp.com>
2013-04-17 21:41:03 -0500

    Used view prefixes.

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,18 +4,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('tagz.views',
     # Examples:
-    url(r'^tagz/lib/$', 'tagz.views.lib'),
+    url(r'^tagz/lib/$', 'lib'),
     url(r'^tagz/lib/(?P<res_name>[^\/]+)/(?P<ref_str>[^\/]+)?$', 
-          'tagz.views.lib_resource'),
-    url(r'^tagz/tags/$', 'tagz.views.tags'),
-    url(r'^tagz/tags/(?P<tag_name>[^\/]+)/$', 'tagz.views.tag'),
+          'lib_resource'),
+    url(r'^tagz/tags/$', 'tags'),
+    url(r'^tagz/tags/(?P<tag_name>[^\/]+)/$', 'tag'),
     url(r'^tagz/refs/$', 'tagz.views.refs'),
-    url(r'^tagz/refs/(?P<ref_name>[^\/]+)/$', 'tagz.views.ref'),
+    url(r'^tagz/refs/(?P<ref_name>[^\/]+)/$', 'ref'),
     # home
-    url(r'^tagz/$', 'tagz.views.nasb'), # redir to tags
-    url(r'^$', 'tagz.views.nasb'), # redir to tags
+    url(r'^tagz/$', 'nasb'), # redir to tags
+    url(r'^$', 'nasb'), # redir to tags
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
