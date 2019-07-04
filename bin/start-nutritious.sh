48e8633d2654b2756e1cba17b7d3c97b5be4c80c
John Lehmann <john.lehmann@gmail.com>
2019-07-04 07:24:19 -0500

    Update pythonpath

--- bin/start-nutritious.sh
+++ bin/start-nutritious.sh
@@ -3,6 +3,8 @@
 # source bash for path
 . $HOME/.bashrc
 #export /Users/john/prog/ChartDirector/lib:/Users/john/git/IbPy:/Users/john/git/old-svn-repo/jplpy/src:/Users/john/git/old-svn-repo/trading/src:/Users/john/git/textbites:/Users/john/git/pybible
+export PYTHONPATH=$HOME/git/old-svn-repo/jplpy/src:$HOME/git/old-svn-repo/trading/src:$HOME/git/textbites:$HOME/git/pybible
+
 
 cd $HOME/git/nutritious
 
